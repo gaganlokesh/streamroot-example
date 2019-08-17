1ee2c7c92c8bea49eeb434b38005b69244783fe7
Gagan Lokesh <gaganlokesh@gmail.com>
2019-08-17 18:44:59 +0530

    INITIAL COMMIT
    Setting up Streamroot DNA with video.js player.
    https://support.streamroot.io/hc/en-us/articles/360000920994-Video-js

---
+++ app.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function(){ 
+
+  var options = {
+    html5: {
+      hlsjsConfig: {},
+    },
+    dnaConfig: {},
+  };
+
+  var player = videojs("player", options);
+
+}, false);
