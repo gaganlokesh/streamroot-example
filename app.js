3ac4f23b1f9d68cdebad4fccad493be0cf9bed14
Gagan <gaganlokesh@gmail.com>
2019-08-17 20:03:51 +0530

    Added a switch to Enable/Disable P2P upload

--- app.js
+++ app.js
@@ -8,5 +8,23 @@ document.addEventListener('DOMContentLoaded', function(){
   };
 
   var player = videojs("player", options);
+  var p2pSwitch = document.getElementById('p2p-switch');
+
+  p2pSwitch.addEventListener('change', (event) => {
+    if (event.target.checked) {
+      // Enabling P2P Upload
+      setP2PConfig(true)
+    } else {
+      // Disabling P2P Upload
+      setP2PConfig(false)
+    }
+  })
+
+  function setP2PConfig(value){
+    if (Streamroot){
+      Streamroot.instances[0].dnaUploadEnabled = value
+      Streamroot.instances[0].dnaUploadAllowed = value
+    }
+  }
 
 }, false);
